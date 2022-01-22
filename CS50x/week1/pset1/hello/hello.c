20cb91ec1000a730d98b863cc3be013bd3562dd4
jackberrypassionfruit <94641852+jackberrypassionfruit@users.noreply.github.com>
2022-01-22 23:55:11 +0000

    Sat, Jan 22, 2022, 11:55 PM +00:00

--- CS50x/week1/pset1/hello/hello.c
+++ CS50x/week1/pset1/hello/hello.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include <cs50.h>
 
 int main(void)
 {
-    string name = get_string("Please input a string");
+    string name = get_string("Please input a string: ");
     printf("hello, %s\n", name);
 }
