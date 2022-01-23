553ce423d5eb1936b6682942cb3d4e5e5623785e
jackberrypassionfruit <94641852+jackberrypassionfruit@users.noreply.github.com>
2022-01-23 14:35:30 +0000

    Sun, Jan 23, 2022, 2:35 PM +00:00

--- CS50x/week1/pset1/credit/tester.c
+++ CS50x/week1/pset1/credit/tester.c
@@ -11,13 +11,12 @@ int main(void) {
     long long i = get_long_long("Gimme a long: ");
     printf("i is: %lld\n", i);
     int len = getLLLength(i);
-    // Converting Int to String
-    printf("i is: %lld\n", i);
+    printf("len is: %i\n", len);
 
-
+    // Converting Long Long to String
     char str[len];
     sprintf(str, "%lld", i);
-    // printf("str[0] is: %c\n", str[0]);
+    printf("str[0] is: %c\n", str[0]);
 
     if (str[0] == '4' && (len == 3 || len == 4)) {
         printf("fuck yeah\n");
@@ -29,12 +28,11 @@ int main(void) {
 
 int getLLLength(long long num) {
     int len = 0;
-    long long ref = num;
-    printf("ref is: %lld\n", ref);
-    while (ref > 0) {
+    printf("num is: %lld\n", num);
+    while (num > 0) {
         len++;
-        ref /= 10;
-        // printf("num is: %lld\n", num);
+        num /= 10;
+        printf("num is: %lld\n", num);
     }
     return len;
 }
