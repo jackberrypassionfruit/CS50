82618036a228ff4271a531efdf5560052f50519f
jackberrypassionfruit <94641852+jackberrypassionfruit@users.noreply.github.com>
2022-01-23 03:38:54 +0000

    Sun, Jan 23, 2022, 3:38 AM +00:00

--- CS50x/week1/pset1/credit/tester.c
+++ CS50x/week1/pset1/credit/tester.c
@@ -3,22 +3,22 @@
 #include <math.h>
 #include <string.h>
 
-int getLengthOfLong();
+long getLengthOfLong();
 
 int main(void) {
 	long cc = get_long("try me, user: ");
-    printf("Loong is %d\n", cc);
+    // printf("Loong is %d\n", cc);
     int len = getLengthOfLong(cc);
 	printf("%i\n", len);
     return 1;
 }
 
-int getLengthOfLong(loong) {
+long getLengthOfLong(loong) {
     int len = 0;
     while (loong > 0) {
         len++;
         loong = loong / 10;
-        printf("Loong is %d\n", loong);
+        printf("Loong is %ld\n", loong);
         printf("Len is %i\n", len);
     }
     return len;
