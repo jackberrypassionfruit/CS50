33789a2e8634b94502fc6a946cd1b3697045589d
jackberrypassionfruit <94641852+jackberrypassionfruit@users.noreply.github.com>
2022-01-23 00:37:46 +0000

    Sun, Jan 23, 2022, 12:37 AM +00:00

--- CS50x/week1/pset1/mario-more/mario.c
+++ CS50x/week1/pset1/mario-more/mario.c
@@ -3,28 +3,34 @@
 
 
 int main(void) {
-    int height = get_input("Please, dear Yahweh please, give me a number: ");
-    for (int i = 1; i < height; i++) {
-        printf()
-    })
+	int height = get_input("Please, dear Yahweh please, give me a number: ");
+	for (int i = 1; i <= height; i++) {
+		drawEmpties(lvl, height);
+		drawBlock(lvl, height);
+		printf("  ");
+		drawBlocks(lvl, height);
+		drawEmpties(lvl, height);
+		printf("\n");
+	})
 }
 
 // Prints each individual Line of blocks
-int drawEmpties(lvl) {
-    for (int i = 1; i < (height - 1); i++) {
-        printf(" ");
-    }
+int drawEmpties(lvl, height) {
+	for (int i = 0; i < (height - lvl); i++) {
+		printf(' ');
+	}
 }
 
-int drawBlocks(lvl) {
-    for (int i = 1; i < lvl; i++) {
-
+int drawBlocks(lvl, height) {
+	for (int i = 0; i < lvl; i++) {
+		printf('#');
 }
 
 /* Pseudocode
 ask for HEIGHT from user
 for i in rows:
-    print empty spaces for the amount of height minus the level
-    print as many hashes as the level
-    print 2 empty spaces
-    # do the last two things, but in reverse order
+	print empty spaces for the amount of height minus the level
+	print as many hashes as the level
+	print 2 empty spaces
+	# do the last two things, but in reverse order
+*/
