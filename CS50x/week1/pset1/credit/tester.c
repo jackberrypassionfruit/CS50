45719d934bacc829e9d9d714e0da66c2db6e3b28
jackberrypassionfruit <94641852+jackberrypassionfruit@users.noreply.github.com>
2022-01-23 10:59:31 +0000

    Sun, Jan 23, 2022, 10:59 AM +00:00

--- CS50x/week1/pset1/credit/tester.c
+++ CS50x/week1/pset1/credit/tester.c
@@ -5,6 +5,11 @@
 
 int main(void) {
     long num = get_long("gimme a long: ");
+    int len = 0;
     printf("Long after input is: %ld\n", num);
+    while (num > 0) {
+        len++;
+        num /= 10;
+    }
     return 1;
 }
