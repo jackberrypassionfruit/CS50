c926501ebcf0c37652b8419f921729a582bd5f48
jackberrypassionfruit <94641852+jackberrypassionfruit@users.noreply.github.com>
2022-01-23 14:36:50 +0000

    Sun, Jan 23, 2022, 2:36 PM +00:00

--- CS50x/week1/pset1/credit/tester.c
+++ CS50x/week1/pset1/credit/tester.c
@@ -8,14 +8,14 @@ int getLLLength();
 // Testing Conditionals in C
 
 int main(void) {
-    long long i = get_long_long("Gimme a long: ");
-    printf("i is: %lld\n", i);
+    long i = get_long("Gimme a long: ");
+    printf("i is: %ld\n", i);
     int len = getLLLength(i);
     printf("len is: %i\n", len);
 
     // Converting Long Long to String
     char str[len];
-    sprintf(str, "%lld", i);
+    sprintf(str, "%ld", i);
     printf("str[0] is: %c\n", str[0]);
 
     if (str[0] == '4' && (len == 3 || len == 4)) {
@@ -26,13 +26,13 @@ int main(void) {
     }
 }
 
-int getLLLength(long long num) {
+int getLLLength(long num) {
     int len = 0;
-    printf("num is: %lld\n", num);
+    printf("num is: %ld\n", num);
     while (num > 0) {
         len++;
         num /= 10;
-        printf("num is: %lld\n", num);
+        printf("num is: %ld\n", num);
     }
     return len;
 }
