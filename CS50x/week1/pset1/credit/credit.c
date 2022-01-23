796d6529518c633122931881428d9a4a6f6f4c0e
jackberrypassionfruit <94641852+jackberrypassionfruit@users.noreply.github.com>
2022-01-23 01:36:47 +0000

    Sun, Jan 23, 2022, 1:36 AM +00:00

--- CS50x/week1/pset1/credit/credit.c
+++ CS50x/week1/pset1/credit/credit.c
@@ -1,3 +1,18 @@
 #include <stdio.h>
 #include <cs50.h>
 
+/* Notes
+Credit Card Identitifiers
+	American Express
+		15 Digit Numbers
+		Starts with 34 or 37
+	MasterCard
+		16 Digit Numbers
+		Starts
+	Visa
+		16 or 13 Digit Numbers
+*/
+
+int main(void) {
+
+}
