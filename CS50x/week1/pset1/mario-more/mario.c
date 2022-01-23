2a3b11a580b90c7a854023f2723043cbea729ea8
jackberrypassionfruit <94641852+jackberrypassionfruit@users.noreply.github.com>
2022-01-23 00:19:25 +0000

    Sun, Jan 23, 2022, 12:19 AM +00:00

--- CS50x/week1/pset1/mario-more/mario.c
+++ CS50x/week1/pset1/mario-more/mario.c
@@ -1,1 +1,27 @@
+#include <stdio.h>
+#include <cs50.h>
 
+
+int main(void) {
+    int height = get_input("Please, dear Yahweh please, give me a number: ");
+    for (int i = 1; i < height; i++) {
+        printf()
+    })
+}
+
+// Prints each individual Line of blocks
+int drawLine(lvl) {
+    for (int i = 1; i < lvl; i++) {
+        // Draw left side
+        for (int i = 1; i < lvl; i++) {
+            if
+    }
+}
+
+/* Pseudocode
+as for HEIGHT from user
+for i in rows:
+    print empty spaces for the amount of height minus the level
+    print as many astericks as the level
+    print 2 empty spaces
+    # do the last two things, but in reverse order
