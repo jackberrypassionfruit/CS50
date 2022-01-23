cd5e708ed4dae68610f7d40e4d5213f304358823
jackberrypassionfruit <94641852+jackberrypassionfruit@users.noreply.github.com>
2022-01-23 11:45:42 +0000

    Sun, Jan 23, 2022, 11:45 AM +00:00

--- CS50x/week1/pset1/credit/tester.c
+++ CS50x/week1/pset1/credit/tester.c
@@ -15,7 +15,7 @@ int main(void) {
 
     char str[len];
     sprintf(str, "%i", i);
-    printf("Int[0] as a string: %c\n", str[0]);
+    printf("str[0] is: %c\n", str[0]);
 
     if ((str[0] == 4) && (len == 3)) {
         printf("fuck yeah\n");
