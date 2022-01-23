abf3bc934f68fe4fb8a5afe5d9d92a8a31df75b5
jackberrypassionfruit <94641852+jackberrypassionfruit@users.noreply.github.com>
2022-01-23 14:03:37 +0000

    Sun, Jan 23, 2022, 2:03 PM +00:00

--- CS50x/week1/pset1/credit/tester.c
+++ CS50x/week1/pset1/credit/tester.c
@@ -8,14 +8,14 @@ int getIntLength();
 // Testing Conditionals in C
 
 int main(void) {
-    long i = get_long("Gimme a long: ");
+    double i = get_double("Gimme a long: ");
     int len = getIntLength(i);
     printf("len is: %i\n", len);
     // Converting Int to String
 
     char str[len];
     sprintf(str, "%ld", i);
-    printf("str[0] is: %c\n", str[0]);
+    printf("str[0] is: %d\n", str[0]);
 
     if (str[0] == '4' && (len == 3 || len == 4)) {
         printf("fuck yeah\n");
