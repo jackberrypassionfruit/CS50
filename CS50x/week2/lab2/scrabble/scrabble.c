68c93c54c3b893b6371a0295577dd174e392be1b
jackberrypassionfruit <94641852+jackberrypassionfruit@users.noreply.github.com>
2022-02-01 02:41:48 +0000

    looking good for submitting

--- CS50x/week2/lab2/scrabble/scrabble.c
+++ CS50x/week2/lab2/scrabble/scrabble.c
@@ -19,9 +19,18 @@ int main(void)
     int score2 = compute_score(word2);
 
     // TODO: Print the winner
-    if (score1 > score2) {printf("Player 1 wins!\n");}
-    else if (score1 < score2) {printf("Player 2 wins!\n");}
-    else {printf("Tie!\n");}
+    if (score1 > score2)
+    {
+        printf("Player 1 wins!\n");
+    }
+    else if (score1 < score2)
+    {
+        printf("Player 2 wins!\n");
+    }
+    else
+    {
+        printf("Tie!\n");
+    }
 }
 
 int compute_score(string word)
