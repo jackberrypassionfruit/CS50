65f9c43c7e25a05ff11c3c6890ccd7bf1182e8bf
jackberrypassionfruit <94641852+jackberrypassionfruit@users.noreply.github.com>
2022-02-01 02:45:53 +0000

    Tue, Feb 1, 2022, 2:45 AM +00:00

--- CS50x/week2/pset2/readability_DONE/readability.c
+++ CS50x/week2/pset2/readability_DONE/readability.c
@@ -10,7 +10,8 @@ float numberOfSentences(string text);
 
 int totLen;
 
-int main(void) {
+int main(void)
+{
     string text = get_string("Text: ");
     totLen = strlen(text);
     float letters = numberOfLetters(text);
@@ -22,20 +23,32 @@ int main(void) {
     // printf("Sentences per 100 words: %f\n", S);
     float level = roundf(0.0588 * L - 0.296 * S - 15.8);
     int ans = level;
-    if (ans > 16) {printf("Grade 16+\n");}
-    else if (ans < 1) {printf ("Before Grade 1\n");}
-    else {printf("Grade Level: %i\n", ans);}
+    if (ans > 16)
+    {
+        printf("Grade 16+\n");
+    }
+    else if (ans < 1)
+    {
+        printf ("Before Grade 1\n");
+    }
+    else
+    {
+        printf("Grade Level: %i\n", ans);
+    }
     return 0;
 }
 
 // DONE
-float numberOfLetters(string text) {
+float numberOfLetters(string text)
+{
     float num = 0;
     char letter;
-    for (int i = 0; i < totLen; i++) {
+    for (int i = 0; i < totLen; i++)
+    {
         letter = text[i];
         // printf("Letter: %c\n", letter);
-        if (toupper(letter) != tolower(letter)) {
+        if (toupper(letter) != tolower(letter))
+        {
             num += 1;
         }
     }
@@ -44,25 +57,35 @@ float numberOfLetters(string text) {
 }
 
 // DONE
-float numberOfWords(string text) {
+float numberOfWords(string text)
+{
     //Starting from 1 to account for the fact that there won't be a space after the last word
     float num = 1;
     char letter;
-    for (int i = 0; i < totLen; i++) {
+    for (int i = 0; i < totLen; i++)
+    {
         letter = text[i];
-        if (letter == ' ') {num += 1;}
+        if (letter == ' ')
+        {
+            num += 1;
+        }
     }
     // printf("Number of words is: %f\n", num);
     return num;
 }
 
 // DONE
-float numberOfSentences(string text) {
+float numberOfSentences(string text)
+{
     float num = 0;
     char letter;
-    for (int i = 0; i < totLen; i++) {
+    for (int i = 0; i < totLen; i++)
+    {
         letter = text[i];
-        if (letter == '.' || letter == '?' || letter == '!') {num += 1;}
+        if (letter == '.' || letter == '?' || letter == '!')
+        {
+            num += 1;
+        }
     }
     // printf("Number of sentences is: %f\n", num);
     return num;
