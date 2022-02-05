ebda4e0f4dbbef2ef3a34a1e4ac569460be1d80c
jackberrypassionfruit <94641852+jackberrypassionfruit@users.noreply.github.com>
2022-02-05 00:52:41 +0000

    Sat, Feb 5, 2022, 12:52 AM +00:00

---
+++ CS50x/week3/pset3/plurality/tester.c
@@ -0,0 +1,9 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, string argv[])
+{
+    printf("Are the string the same?: %i\n", strcmp(argv[1], argv[2]));
+    return 0;
+}
