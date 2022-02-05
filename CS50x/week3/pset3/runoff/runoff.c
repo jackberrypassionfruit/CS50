ebf5631a6193df60c2b6dd32c5111d23ec5a0710
jackberrypassionfruit <94641852+jackberrypassionfruit@users.noreply.github.com>
2022-02-05 02:46:15 +0000

    Sat, Feb 5, 2022, 2:46 AM +00:00

--- CS50x/week3/pset3/runoff/runoff.c
+++ CS50x/week3/pset3/runoff/runoff.c
@@ -142,7 +142,10 @@ bool vote(int voter, int rank, string name)
 // Tabulate votes for non-eliminated candidates
 void tabulate(void)
 {
-    // TODO
+    for (int i = 0; i < candidate_count; i++)
+    {
+
+    }
     return;
 }
 
