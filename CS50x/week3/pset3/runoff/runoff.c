cecf9d0a32f782128c1f31602428e3b4dc42f8b0
jackberrypassionfruit <94641852+jackberrypassionfruit@users.noreply.github.com>
2022-02-05 02:43:41 +0000

    Sat, Feb 5, 2022, 2:43 AM +00:00

--- CS50x/week3/pset3/runoff/runoff.c
+++ CS50x/week3/pset3/runoff/runoff.c
@@ -127,7 +127,14 @@ int main(int argc, string argv[])
 // Record preference if vote is valid
 bool vote(int voter, int rank, string name)
 {
-    // TODO
+    for (int i = 0; i < candidate_count; i++)
+    {
+        if (!strcmp(name, candidates[i].name))
+        {
+            preferences[voter][rank] = name;
+            return true;
+        }
+    }
     return false;
 }
 
