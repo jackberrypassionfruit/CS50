678a321883386a9d9e72c02f66a748b689eec642
jackberrypassionfruit <94641852+jackberrypassionfruit@users.noreply.github.com>
2022-02-06 00:40:28 +0000

    Sun, Feb 6, 2022, 12:40 AM +00:00

---
+++ CS50x/week3/pset3/runoff/tester.c
@@ -0,0 +1,12 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+
+int main(void)
+{
+    int x = 4;
+    int y = 93;
+    float z = y /  x;
+    printf("x / y = %f\n", z);
+}
