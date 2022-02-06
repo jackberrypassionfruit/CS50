e68ef3abda7dd9c624f5e76c28b40c9f4a579bd4
jackberrypassionfruit <94641852+jackberrypassionfruit@users.noreply.github.com>
2022-02-06 01:01:45 +0000

    Sun, Feb 6, 2022, 1:01 AM +00:00

--- CS50x/week3/pset3/runoff/runoff.c
+++ CS50x/week3/pset3/runoff/runoff.c
@@ -206,7 +206,8 @@ bool print_winner(void)
 // Return the minimum number of votes any remaining candidate has
 int find_min(void)
 {
-    int min_votes MAX;
+    // Technically, the maximum number of votes a candidate can have. It can only be lower
+    int min_votes = MAX_CANDIDATES;
     for (int i = 0; i < candidate_count; i++)
     {
         if (candidates[i].votes < min_votes)
