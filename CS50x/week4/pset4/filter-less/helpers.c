ff62eb9fa46027ff7048bde5f1ad95ee7109bab2
jackberrypassionfruit <94641852+jackberrypassionfruit@users.noreply.github.com>
2022-02-09 18:26:33 +0000

    Wed, Feb 9, 2022, 6:26 PM +00:00

--- CS50x/week4/pset4/filter-less/helpers.c
+++ CS50x/week4/pset4/filter-less/helpers.c
@@ -14,11 +14,11 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
             int g = image[i][j].rgbtGreen;
             int b = image[i][j].rgbtBlue;
 
-            float rgb_avg = (float) (r + g + b) / 3.0;
+            int rgb_avg = ceil((float) (r + g + b) / 3.0);
 
-            image[i][j].rgbtRed = (int) rgb_avg;
-            image[i][j].rgbtGreen = (int) rgb_avg;
-            image[i][j].rgbtBlue = (int) rgb_avg;
+            image[i][j].rgbtRed = rgb_avg;
+            image[i][j].rgbtGreen = rgb_avg;
+            image[i][j].rgbtBlue = rgb_avg;
         }
     }
     return;
