43d69fc6e01354bed1e834b10cd93ca65f946d2d
jackberrypassionfruit <94641852+jackberrypassionfruit@users.noreply.github.com>
2022-02-09 19:22:33 +0000

    Wed, Feb 9, 2022, 7:22 PM +00:00

--- CS50x/week4/pset4/filter-less/helpers.c
+++ CS50x/week4/pset4/filter-less/helpers.c
@@ -74,9 +74,9 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
         }
         for (int j = 0; j < width; j++)
         {
-            image[i][j].rgbtRed = buffer[j + 1].rgbtRed;
-            image[i][j].rgbtGreen = buffer[j + 1].rgbtGreen;
-            image[i][j].rgbtBlue = buffer[j + 1].rgbtBlue;
+            image[i][j].rgbtRed = buffer[j].rgbtRed;
+            image[i][j].rgbtGreen = buffer[j].rgbtGreen;
+            image[i][j].rgbtBlue = buffer[j].rgbtBlue;
         }
     }
     return;
