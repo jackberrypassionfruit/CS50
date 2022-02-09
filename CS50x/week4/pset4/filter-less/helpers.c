d90361fbbb7e6956b1ca8e677f8d181b8df98e1c
jackberrypassionfruit <94641852+jackberrypassionfruit@users.noreply.github.com>
2022-02-09 02:21:15 +0000

    Wed, Feb 9, 2022, 2:21 AM +00:00

--- CS50x/week4/pset4/filter-less/helpers.c
+++ CS50x/week4/pset4/filter-less/helpers.c
@@ -59,7 +59,7 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
-    int buffer[width];
+    BYTE buffer[width];
 
     for (int i = 0; i < height; i++)
     {
