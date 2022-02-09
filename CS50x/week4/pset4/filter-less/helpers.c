e88d7b726c6f34e80a5ba67b5a81d8f2c575334c
jackberrypassionfruit <94641852+jackberrypassionfruit@users.noreply.github.com>
2022-02-09 01:52:58 +0000

    Wed, Feb 9, 2022, 1:52 AM +00:00

--- CS50x/week4/pset4/filter-less/helpers.c
+++ CS50x/week4/pset4/filter-less/helpers.c
@@ -3,6 +3,21 @@
 // Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j ++)
+        {
+            int r = image[i][j].rgbtRed;
+            int g = image[i][j].rgbtGreen;
+            int b = image[i][j].rgbtBlue;
+
+            int rgb_avg = (r + g + b) / 3;
+
+            image[i][j].rgbtRed = rgb_avg;
+            image[i][j].rgbtGreen = rgb_avg;
+            image[i][j].rgbtBlue = rgb_avg;
+        }
+    }
     return;
 }
 
