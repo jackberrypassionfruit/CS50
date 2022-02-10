1b732790397f9bffa7dce3a2ed7034cea06635a4
jackberrypassionfruit <94641852+jackberrypassionfruit@users.noreply.github.com>
2022-02-10 20:05:18 +0000

    Thu, Feb 10, 2022, 8:05 PM +00:00

--- CS50x/week4/pset4/recover/recover.c
+++ CS50x/week4/pset4/recover/recover.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
- 
+
+
+
 int main(int argc, char *argv[])
 {
- 
+    // Check command-line arguments
+    if (argc != 2)
+    {
+        printf("Usage: ./recover card.raw\n");
+        return 1;
+    }
 }
