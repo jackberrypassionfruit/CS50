ead3fde4c0310238515b8cf069d3c871094b7275
jackberrypassionfruit <94641852+jackberrypassionfruit@users.noreply.github.com>
2022-02-11 00:46:37 +0000

    Fri, Feb 11, 2022, 12:46 AM +00:00

--- CS50x/week4/pset4/recover/recover.c
+++ CS50x/week4/pset4/recover/recover.c
@@ -13,8 +13,8 @@ int main(int argc, char *argv[])
     }
 
     // Open card.raw file
-    FILE *input = fopen(argv[1], "r");
-    if (input == NULL)
+    FILE *card = fopen(argv[1], "r");
+    if (card == NULL)
     {
         printf("Could not open file.\n");
         return 1;
@@ -24,12 +24,13 @@ int main(int argc, char *argv[])
 
 
     int i = 0
-    while (fread(&block, sizeof(uint8_t), 512, input))
+    while (fread(&block, sizeof(uint8_t), 512, card))
     {
-        if (block[0] == 0xff && block[1] == 0xd8 && block[2] == 0xff && block[3] >= 224 && block[3] <= 239)
+        if (block[0] == 0xff && block[1] == 0xd8 && block[2] == 0xff && (block[3] >= 224) && (block[3] <= 239))
         {
+            FILE *output = fopen("%03i.jpg", "r");
+            i++;
 
-            FILE *output = fopen(
 
         }
     }
