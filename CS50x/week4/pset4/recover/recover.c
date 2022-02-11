921fc485c7d18a20a26f063104b1af3d700bff7b
jackberrypassionfruit <94641852+jackberrypassionfruit@users.noreply.github.com>
2022-02-11 01:03:20 +0000

    Fri, Feb 11, 2022, 1:03 AM +00:00

--- CS50x/week4/pset4/recover/recover.c
+++ CS50x/week4/pset4/recover/recover.c
@@ -27,13 +27,13 @@ int main(int argc, char *argv[])
     char name[7];
 
     // Hopfully block is rest with new info every time this next fread function is called
-    while (fread(&block, sizeof(uint8_t), 512, card))
+    while (fread(&block, sizeof(char), 512, card))
     {
         if (block[0] == 0xff && block[1] == 0xd8 && block[2] == 0xff && (block[3] >= 224) && (block[3] <= 239))
         {
             sprintf(name, "%03i.jpg\n", i);
             FILE *output = fopen(name,  "r");
-            fwrite(&block, sizeof(uint8_t), 512, output);
+            fwrite(&block, sizeof(char), 512, output);
             fclose(output);
             i++;
         }
