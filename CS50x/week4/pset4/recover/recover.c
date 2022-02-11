8a25eb14914ebe446b6a0210f57aee56d34666f6
jackberrypassionfruit <94641852+jackberrypassionfruit@users.noreply.github.com>
2022-02-11 23:51:53 +0000

    Fri, Feb 11, 2022, 11:51 PM +00:00

--- CS50x/week4/pset4/recover/recover.c
+++ CS50x/week4/pset4/recover/recover.c
@@ -31,11 +31,11 @@ int main(int argc, char *argv[])
     This is because I write to the jpegs only when I see the start of a new one.
     I stop writing, and leave off the rest this way.
 
-    What I SHOULD do, is every time I see te start condition, close the outp[ut file I had been writing, iterate the filename, and open a newfile for the new jpeg.
+    What I SHOULD do, is every time I see the start condition, close the output file I had been writing, iterate the filename, and open a newfile for the new jpeg.
     This is the file I will continue writing to until the new beginning.
     */
 
-    while (fread(&block, 512, 1, card) == 1)
+    while (fread(&block, sizeof(uint8_t), 512, card) == 512)
     {
         printf("blocks [1], [2], and [3] are: %x, %x,  %x, and %x\n", block[0], block[1], block[2], block[3]);
         if (block[0] == 0xff && block[1] == 0xd8 && block[2] == 0xff && (block[3] >= 0xe0) && (block[3] <= 0xef))
