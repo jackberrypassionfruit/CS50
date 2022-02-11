f24fa09046afb541aa0e1d55124518f9f02f7aba
jackberrypassionfruit <94641852+jackberrypassionfruit@users.noreply.github.com>
2022-02-11 01:35:34 +0000

    Fri, Feb 11, 2022, 1:35 AM +00:00

--- CS50x/week4/pset4/recover/tester.c
+++ CS50x/week4/pset4/recover/tester.c
@@ -2,10 +2,5 @@
 
 int main(int argc, char *argv[])
 {
-    char name[7];
-    for (int i = 0; i < 5; i++)
-    {
-        sprintf(name, "%03i.jpg\n", i);
-        printf("%s", name);
-    }
+    printf("i\n", 300 > 0xe0);
 }
