ff02ab8e9e2ea1105af31981cddc6f8b1a0d763e
jackberrypassionfruit <94641852+jackberrypassionfruit@users.noreply.github.com>
2022-02-12 17:50:11 +0000

    Sat, Feb 12, 2022, 5:50 PM +00:00

--- CS50x/week5/lab5/inheritance/inheritance.c
+++ CS50x/week5/lab5/inheritance/inheritance.c
@@ -39,13 +39,14 @@ int main(void)
 // Create a new individual with `generations`
 person *create_family(int generations)
 {
-    // TODO: Allocate memory for new person
+    // TODO_DONE: Allocate memory for new person
     person *child = malloc(sizeof(person));
     if (child == NULL)
     {
         printf("No room in memory for new person.\n");
         return;
     }
+
     // If there are still generations left to create
     if (generations > 1)
     {
@@ -53,7 +54,10 @@ person *create_family(int generations)
         person *parent0 = create_family(generations - 1);
         person *parent1 = create_family(generations - 1);
 
-        // TODO: Set parent pointers for current person
+        // TODO_DONE: Set parent pointers for current person
+        child.person[0] = parent0;
+        child.person[1] = parent1;
+
 
         // TODO: Randomly assign current person's alleles based on the alleles of their parents
 
