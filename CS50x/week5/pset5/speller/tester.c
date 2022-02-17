190b45f223834d6bef11778b9c13a7de2e54e53d
jackberrypassionfruit <94641852+jackberrypassionfruit@users.noreply.github.com>
2022-02-17 00:27:26 +0000

    Thu, Feb 17, 2022, 12:27 AM +00:00

--- CS50x/week5/pset5/speller/tester.c
+++ CS50x/week5/pset5/speller/tester.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void)
+int main(int argc, char *argv[])
 {
+    FILE *dict = fopen(argv[1]);
+    if (dict == NULL)
+    {
+        printf("dictionary couldn't load\n");
+        return 1;
+    }
+
+    FILE *word = fopen("word.txt", "w");
+    while (fscanf(dict, "%s", word) == EOF)
+    return 0;
 
 }
