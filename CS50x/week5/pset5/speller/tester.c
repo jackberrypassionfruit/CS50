72819184eeeb4ebb398d7737afcc4818d1274cb1
jackberrypassionfruit <94641852+jackberrypassionfruit@users.noreply.github.com>
2022-02-17 00:49:44 +0000

    Thu, Feb 17, 2022, 12:49 AM +00:00

--- CS50x/week5/pset5/speller/tester.c
+++ CS50x/week5/pset5/speller/tester.c
@@ -1,20 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char *argv[])
 {
     FILE *dict = fopen(argv[1], "r");
-    if (dict == NULL)
+    char *word[46];
+    while (fscan(dict, "%s", word))
     {
-        printf("dictionary couldn't load\n");
-        return 1;
+        if (!(str))
     }
 
-    char word[46];
-    while (fscanf(dict, "%s", word) != EOF)
-    {
-        printf("word is: %s\n", word);
-    }
+
+
+
     return 0;
 
 }
