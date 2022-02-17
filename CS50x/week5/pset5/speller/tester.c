e0dabce3ce2aaa03f790500180bbbf5a6b514ad0
jackberrypassionfruit <94641852+jackberrypassionfruit@users.noreply.github.com>
2022-02-17 00:22:27 +0000

    Thu, Feb 17, 2022, 12:22 AM +00:00

--- CS50x/week5/pset5/speller/tester.c
+++ CS50x/week5/pset5/speller/tester.c
@@ -3,15 +3,5 @@
 
 int main(void)
 {
-    char stack[] = "hello";
-    char *heap = "hello";
 
-    char *heap_string_malloc = malloc(6);
-    sprintf(heap_string_malloc, "hello");
-
-    printf("Address of stack[0]: %p\n", stack);
-    printf("Address of heap[0]: %p\n", heap);
-    printf("Address of heap_string_malloc[0]: %p\n", heap_string_malloc);
-
-    free(heap_string_malloc);
 }
