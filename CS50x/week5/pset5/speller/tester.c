df73603974bbd571b2272312722d2e64284a29d8
jackberrypassionfruit <94641852+jackberrypassionfruit@users.noreply.github.com>
2022-02-17 03:01:49 +0000

    Thu, Feb 17, 2022, 3:01 AM +00:00

--- CS50x/week5/pset5/speller/tester.c
+++ CS50x/week5/pset5/speller/tester.c
@@ -4,5 +4,18 @@
 
 int main(int argc, char *argv[])
 {
+    int len = strlen(argv[1]);
+    unsigned int hash_num = 0;
 
+    if (len > 6)
+    {
+        len = 6;
+    }
+
+    for (int i = 0; i < len; i++)
+    {
+        hash_num += ((int) word[i]) * pow(26, i + 1);
+    }
+
+    return hash_num;
 }
