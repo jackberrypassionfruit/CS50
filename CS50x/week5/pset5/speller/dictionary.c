83d0c77a2a3d671829844d357d16beae7a024fcc
jackberrypassionfruit <94641852+jackberrypassionfruit@users.noreply.github.com>
2022-02-18 01:47:05 +0000

    Fri, Feb 18, 2022, 1:47 AM +00:00

--- CS50x/week5/pset5/speller/dictionary.c
+++ CS50x/week5/pset5/speller/dictionary.c
@@ -129,7 +129,7 @@ bool load(const char *dictionary)
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
 unsigned int size(void)
 {
-    // TODO
+    // TODO_DONE
     unsigned int count = 0;
     node *pntr;
 
