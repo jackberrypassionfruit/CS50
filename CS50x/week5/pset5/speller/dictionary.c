8330af7776f350779eeb5e559f3ad645f07fc80c
jackberrypassionfruit <94641852+jackberrypassionfruit@users.noreply.github.com>
2022-02-18 13:17:45 +0000

    Fri, Feb 18, 2022, 1:17 PM +00:00

--- CS50x/week5/pset5/speller/dictionary.c
+++ CS50x/week5/pset5/speller/dictionary.c
@@ -68,6 +68,10 @@ unsigned int hash(const char *word)
     for (int i = 0; i < len; i++)
     {
         int alph_index = tolower(word[i]) % 96;
+        if (word[i] == '\'')
+        {
+            alph_index = 27;
+        }
         hash_num += (alph_index * pow(26, i));
     }
 
--- CS50x/week5/pset5/speller/tester.c
+++ CS50x/week5/pset5/speller/tester.c
@@ -8,7 +8,7 @@
 
 int main(int argc, char *argv[])
 {
-    int hash_index = hash("bbbeyzs");
+    int hash_index = hash("'");
     printf("%i\n", hash_index);
     return 1;
 }
