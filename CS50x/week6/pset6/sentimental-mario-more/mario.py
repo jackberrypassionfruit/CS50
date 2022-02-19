cae4303b3e43e3a844d18390fc73f9e8a2bd34d5
jackberrypassionfruit <pashayanjack@gmail.com>
2022-02-19 01:05:22 -0500

    After working on the first python pset a bit, now on readability

---
+++ CS50x/week6/pset6/sentimental-mario-more/mario.py
@@ -0,0 +1,9 @@
+while True:
+    height = int(input("Height of tower?: "))
+    if height > 2 and height < 8:
+        break
+
+for i in range(height):
+    print(" " * (height - i - 1), end="")
+    print("#" * (i + 1), end=" ")
+    print("#" * (i + 1))
