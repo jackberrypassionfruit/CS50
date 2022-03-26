540e2c0956529d48ffca58240bcdf2844d372e8e
jackberrypassionfruit <94641852+jackberrypassionfruit@users.noreply.github.com>
2022-03-26 11:19:09 +0000

    Sat, Mar 26, 2022, 11:19 AM +00:00

--- CS50x/week7/pset7/movies/13.sql
+++ CS50x/week7/pset7/movies/13.sql
@@ -1,12 +1,15 @@
-SELECT name FROM people, stars
-ON people.id = stars.person_id
-WHERE movies.id IN
-
-(SELECT movie_id FROM movies, stars, people
-ON movies.id = stars.movie_id
-AND stars.person_id = people.id
-WHERE people.name LIKE "%Kevin Bacon%");
-
 -- list the names of all people who starred in a movie in which Kevin Bacon also starred.
+
+SELECT
+
+
+-- SELECT name FROM people, stars
+-- ON people.id = stars.person_id
+-- WHERE movies.id IN
+
+-- (SELECT movie_id FROM movies, stars, people
+-- ON movies.id = stars.movie_id
+-- AND stars.person_id = people.id
+-- WHERE people.name LIKE "%Kevin Bacon%");
 
 -- DIDnt finish this one, not sure about this error tbh. Kinda dumb
